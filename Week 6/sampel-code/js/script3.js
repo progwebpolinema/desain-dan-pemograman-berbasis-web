b5df05aee3b2e537df48721f4f2f63c44087597f
afif hendrawan <afif.hendrawan@gmail.com>
2018-04-09 14:32:03 +0700

    Add Week 6 - jQuery

---
+++ Week 6/sampel-code/js/script3.js
@@ -0,0 +1,17 @@
+function validateForm () {
+    var fname = document.forms["myForm"]["fname"].value;
+    var lname = document.forms["myForm"]["lname"].value;
+
+    if (fname == "" && lname == "") {
+        alert('Nama depan dan belakang harus di isi!');
+        return false;
+    } else if (fname == "") {
+        alert('Nama depan harus di isi!');
+        return false;
+    } else if (lname == "") {
+        alert('Nama belakang harus di isi!');
+        return false;
+    } else {
+        return true;
+    }
+}
