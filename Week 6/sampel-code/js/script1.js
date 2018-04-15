c4a44c6a7920e0b25c38b39db83ffa8de20ade95
afif hendrawan <afif.hendrawan@gmail.com>
2018-04-15 21:08:33 +0700

    delete unecessary files

--- Week 6/sampel-code/js/script1.js
+++
@@ -1,8 +0,0 @@
-function getValue() {
-    var firstName = document.getElementById('firstName').value;
-    var lastName = document.getElementById('lastName').value;
-    var par = document.createElement('p');
-    var node = document.createTextNode('Hallo ' + firstName + ' ' +lastName);
-    par.appendChild(node);
-    document.getElementById('div').appendChild(par);
-}
